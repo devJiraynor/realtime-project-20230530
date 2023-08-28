d022c105893f731cfbf4cbeae7705d006dcd63dd
devJiraynor <jiraynor@naver.com>
2023-08-28 15:19:39 +0900

    2023.08.28. 15:20

--- realtime-front/src/views/Enter/index.tsx
+++ realtime-front/src/views/Enter/index.tsx
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, { ChangeEvent, KeyboardEvent, useRef } from 'react';
 import './style.css';
+import { usePathStore, useRoomStore } from '../../stores';
 
+//          component: 방 접속 화면 컴포넌트          //
 export default function Enter() {
 
+  //          state: Enter Button Ref 상태          //
+  const enterButtonRef = useRef<HTMLDivElement | null>(null);
+  //          state: path 상태 변경          //
+  const { setPath } = usePathStore();
+  //          state: room 상태 및 변경 함수          //
+  const { room, setRoom } = useRoomStore();
 
+  //          event handler: room 값 변경 처리          //
+  const onRoomValueChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    const room = event.target.value;
+    setRoom(room);
+  }
+  //          event handler: 뒤로가기 버튼 클릭 처리          //
+  const onBackButtonClickHandler = () => {
+    setRoom('');
+    setPath('/');
+  }
+  //          event handler: 접속 버튼 클릭 처리          //
+  const onEnterButtonClickHandler = () => {
+    if (!room) return;
+    setPath('/room');
+  }
+  //          event handler: Enter Key 누름 처리          //
+  const onEnterKeyDownHandler = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key !== 'Enter') return;
+    if (!enterButtonRef.current) return;
+    enterButtonRef.current.click();
+  }
+
+  //          render: 방 접속 화면 컴포넌트 렌더링          //
   return (
     <div id='enter-wrapper'>
-      <div className='enter-back-button'>뒤로가기</div>
+      <div className='enter-back-button' onClick={onBackButtonClickHandler}>뒤로가기</div>
       <div className='enter-input-box'>
-        <input className='enter-input' type='text' placeholder='방 번호를 입력하세요.' />
-        <div className='enter-button'>들어가기</div>
+        <input className='enter-input' type='text' placeholder='방 번호를 입력하세요.' value={room} onChange={onRoomValueChangeHandler} onKeyDown={onEnterKeyDownHandler} />
+        <div ref={enterButtonRef} className='enter-button' onClick={onEnterButtonClickHandler}>들어가기</div>
       </div>
     </div>
   )
--- realtime-front/src/views/Room/index.tsx
+++ realtime-front/src/views/Room/index.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import './style.css';
+import { usePathStore, useRoomStore } from '../../stores';
 
-export default function index() {
+export default function Room() {
+
+  const { setPath } = usePathStore();
+  const { room, setRoom } = useRoomStore();
+
   return (
-    <div>index</div>
+    <div id='room'>
+      <div className='room-header'>
+        <div className='room-number'>{room}</div>
+        <div className='room-back-button'>뒤로가기</div>
+      </div>
+      <div className='room-container'></div>
+      <div className='room-footer'></div>
+    </div>
   )
 }
