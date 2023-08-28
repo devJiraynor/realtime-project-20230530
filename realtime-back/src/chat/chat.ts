54c0f8b6449ff905b48ff9185c73a1af9f479d5f
devJiraynor <jiraynor@naver.com>
2023-08-28 16:22:52 +0900

    2023.08.28. 16:20

--- realtime-back/src/chat/chat.ts
+++ realtime-back/src/chat/chat.ts
@@ -1,6 +1,7 @@
 import { Logger } from '@nestjs/common';
 import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
+import { MessageDto } from './types';
 
 @WebSocketGateway(4010, { transports: ['websocket', 'polling'], cors: { origin: '*' } })
 export class Chat {
@@ -14,20 +15,28 @@ export class Chat {
     @MessageBody() room: string,
     @ConnectedSocket() socket: Socket
   ): void {
+    this.logger.warn('Join Room ! - ' + JSON.stringify(room));
     socket.join(room);
   }
 
   @SubscribeMessage('send')
-  handleSend(@MessageBody() data: any): void {
-    this.logger.verbose(JSON.stringify(data));
-    this.server.emit('receive', data);
+  handleSend(@MessageBody() data: MessageDto) {
+    const { room } = data;
+    this.logger.warn('Send Event ! - ' + JSON.stringify(data));
+    this.server.to(room).emit('receive', data);
   }
 
-  @SubscribeMessage('sendRoom')
-  handleSendRoom(@MessageBody() data: any): void {
-    this.logger.verbose(JSON.stringify(data));
-    const { room, message } = data;
-    this.server.to(room).emit('roomReceive', message);
-  }
+  // @SubscribeMessage('send')
+  // handleSend(@MessageBody() data: any): void {
+  //   this.logger.verbose(JSON.stringify(data));
+  //   this.server.emit('receive', data);
+  // }
+
+  // @SubscribeMessage('sendRoom')
+  // handleSendRoom(@MessageBody() data: any): void {
+  //   this.logger.verbose(JSON.stringify(data));
+  //   const { room, message } = data;
+  //   this.server.to(room).emit('roomReceive', message);
+  // }
 
 }
--- realtime-front/src/views/Room/index.tsx
+++ realtime-front/src/views/Room/index.tsx
@@ -1,20 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.css';
 import { usePathStore, useRoomStore } from '../../stores';
+import { socket } from '../../utils/socket';
 
+//          component: 채팅방 컴포넌트          //
 export default function Room() {
 
+  //          state: path 상태 변경 함수          //
   const { setPath } = usePathStore();
+  //          state: room 상태 및 변경 함수          //
   const { room, setRoom } = useRoomStore();
+  //          state: 소켓 연결 상태          //
+  const [isSocketConnected, setSocketConnected] = useState<boolean>(socket.connected);
 
+  //          event handler: 뒤로가기 버튼 클릭 처리          //
+  const onBackButtonClickHandler = () => {
+    setPath('/enter');
+  }
+
+  //          effect: 첫 마운트 시 소켓 연결          //
+  let effectFlag = true;
+  useEffect(() => {
+    if (!effectFlag) return;
+    effectFlag = false;
+
+    const onConnect = () => {
+      console.log(socket.id);
+      setSocketConnected(true);
+    }
+
+    const onDisconnect = () => {
+      setSocketConnected(false);
+    }
+
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect);
+
+    socket.emit('join', room);
+  }, []);
+
+  //          render: 채팅방 컴포넌트 render          //
   return (
     <div id='room'>
       <div className='room-header'>
         <div className='room-number'>{room}</div>
-        <div className='room-back-button'>뒤로가기</div>
+        <div className='room-back-button' onClick={onBackButtonClickHandler}>뒤로가기</div>
       </div>
-      <div className='room-container'></div>
-      <div className='room-footer'></div>
+      <div className='room-container'>
+
+      </div>
+      <div className='room-footer'>
+        <input className='room-send-input' type='text' />
+        <div className='room-send-button'>전송</div>
+      </div>
     </div>
   )
 }
